33bae937e114724e5b1692580945ceb5f5ccba5c
Osca-M <oscamwongera@gmail.com>
2021-06-15 11:05:44 +0300

    Introduce gorilla.mux as my default route handler

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
-func main() {
-	handleRequests()
-}
 
-func handleRequests() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":5000", nil))
-}
+
+
 
 func homePage(w http.ResponseWriter, r *http.Request)  {
 	_, _ = fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -22,5 +19,32 @@ func homePage(w http.ResponseWriter, r *http.Request)  {
 	fmt.Println("Endpoint Hit: homePage")
 }
 
+type Article struct {
+	Title string `json:"title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
 
 
+func allArticles(w http.ResponseWriter, r *http.Request)  {
+	fmt.Println("Endpoint Hit: allArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":5000", myRouter))
+}
+
+func main() {
+
+	Articles = []Article{
+		{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	handleRequests()
+}
